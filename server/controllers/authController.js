2eea6727160b69c5616f4ef1a61a48d66d2d8c88
Staleee <145343918+Staleee@users.noreply.github.com>
2024-08-16 12:31:58 +0300

    added backend for register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,46 @@
-
+const User = require('../models/user')
+const {hashPassword, comparePassword} = require('../helpers/auth')
 
 const test = (req, res) => {
     res.json('test is working')
 }
 
+const registerUser = async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        //check if name waas entered
+        if(!name) {
+            return res.json({
+                error: 'name is required'
+            })
+        };
+        //check if password is good 
+        if(!password || password.length < 6){
+            return res.json({
+                error: 'password is required and should be at least 6 characters long'
+            })
+        };
+        //check email
+        const exist = await User.findOne({email})
+        if(exist) {
+            return res.json({
+                error: 'Email is already in use'
+            })
+        }
+
+        const hashedPassword = await hashPassword(password)
+        //create user in db 
+        const user = await User.create({
+            name, email, password: hashedPassword,
+        });
+
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
-    test
+    test,
+    registerUser
 }
--- server/index.js
+++ server/index.js
@@ -1,49 +1,23 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
-// Load environment variables from .env file
+const {mongoose} = require('mongoose');
 dotenv.config();
 
 const app = express();
-app.use(cors());
 
-// Add routes
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Database connected'))
+.catch((err) => console.log('Database not connected',err))
+
+//middleware
+app.use(express.json())
+
+
 app.use('/', require('./routes/authRoutes'));
 
-// Create HTTP server
-const server = http.createServer(app);
-
-// Set up Socket.io server
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
-// Handle Socket.io connections
-io.on('connection', (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on('join_room', (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on('send_message', (data) => {
-    socket.to(data.room).emit('receive_message', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User Disconnected', socket.id);
-  });
-});
-
-// Start server and Socket.io server on different ports
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
